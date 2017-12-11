362696403ce995b25f914124fe242188cc600fdd
l38046 <32620230+l38046@users.noreply.github.com>
2017-12-11 11:06:59 +0000

    Update SecondTest.swift

--- SecondTest.swift
+++ SecondTest.swift
@@ -1,25 +1,25 @@
 func testPlayerMovement(){
   
         let gameScene=GameScene(size: CGSize(width: 2304, height:1440))
-        var posicao = GameScene(playerPosition: CGPoint(x:2304/2-33 y:1440/2))
+        let posicao = GameScene(playerPosition: CGPoint(x:2304/2-33 y:1440/2))
  
         //A
         
-        XCTAssertEqual(gameScene.keyDown.posicao, CGPoint(x:(2304/2)-33,y:1440/2)" Está correto")
+        XCTAssertEqual(gameScene.keyDown.event.keyCode==0, CGPoint(x:(2304/2)-33,y:1440/2)" Está correto")
 
       //D
-       var posicao = GameScene(playerPosition: CGPoint(x:2304/2+33 y:1440/2))
+       
 
-        XCTAssertEqual(gameScene.keyDown.posicao, CGPoint(x:2304/2+33,y:1440/2)" Está correto")
+        XCTAssertEqual(gameScene.keyDown.event.keyCode==2, CGPoint(x:2304/2+33,y:1440/2)" Está correto")
 
      //W
-       var posicao = GameScene(playerPosition: CGPoint(x:2304/2 y:1440/2-33))
+      
 
-        XCTAssertEqual(gameScene.keyDown.posicao, CGPoint(x:(2304/2),y:1440/2-33)" Está correto")
+        XCTAssertEqual(gameScene.keyDown.event.keyCode==13, CGPoint(x:(2304/2),y:1440/2-33)" Está correto")
     
     //S
-         var posicao = GameScene(playerPosition: CGPoint(x:2304/2 y:1440/2+33))
+        
 
-        XCTAssertEqual(gameScene.keyDown.posicao, CGPoint(x:(2304/2),y:1440/2+33)" Está correto")
+        XCTAssertEqual(gameScene.keyDown.event.KeyCode==1, CGPoint(x:(2304/2),y:1440/2+33)" Está correto")
 
 }
