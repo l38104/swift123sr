425c746d92026948c83a78f57774e2c015714ad8
l38046 <32620230+l38046@users.noreply.github.com>
2017-12-13 23:22:42 +0000

    Update ThirdTest.swift

--- ThirdTest.swift
+++ ThirdTest.swift
@@ -2,8 +2,18 @@ func testWalls(){
       
         
       let gameScene=GameScene()
+      
       let leftWall=[[Float(30),Float(27)]]
-      let rightWall=[[Float(2274),Float[27
+      let rightWall=[[Float(2274),Float[27]]
+                                        
+       XCTAssertTrue(gameScene.walls(points:leftWall))
+       XCTAssertTrue(gameScene.walls(points:rightWall))		        
+  		  
+  // VE a primeira parte e depois ve a segunda depois deste comentario e comenta o de cima    excepto o gameScene                             
+                                        
+                                        
+                                        
+                                        
       
       //Esquerda
         for i in stride(from: -6, to: 1479, by: 33) { leftWall.append([Float(30), Float(i)]) }
