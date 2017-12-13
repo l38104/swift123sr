eade1d876992eb6ab7b7234f25676f89dbc9bdaa
l38847 <32617339+l38847@users.noreply.github.com>
2017-12-13 23:17:20 +0000

    Update ThirdTest.swift

--- ThirdTest.swift
+++ ThirdTest.swift
@@ -3,7 +3,13 @@ func testWalls(){
         
       let gameScene=GameScene()
       let leftWall=[[Float(30),Float(27)]]
-      let rightWall=[[Float(2274),Float[27]]
+      let rightWall=[[Float(2274),Float[27
+      
+      //Esquerda
+        for i in stride(from: -6, to: 1479, by: 33) { leftWall.append([Float(30), Float(i)]) }
+        
+        //Direita
+        for i in stride(from: -6, to: 1479, by: 33) {rightWall.append([Float(2274), Float(i)]) }                                  
 
 
 
