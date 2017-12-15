62aae5b0168ded74d86162fb20daaac75c9c9337
l38104 <32617112+l38104@users.noreply.github.com>
2017-12-15 09:53:03 +0000

    Update ThirdTest.swift

--- ThirdTest.swift
+++ ThirdTest.swift
@@ -16,15 +16,15 @@ func testWalls(){
                                         
       
       //Esquerda
-        for i in stride(from: -6, to: 1479, by: 33) { leftWall.append([Float(30), Float(i)]) }
+        for i in stride(from: -6, to: 1479, by: 33) { leftWall.append([Float(30), Float(i)]) } - Cannot use mutating member on immutable value: 'leftWall' is a 'let' constant
         
         //Direita
-        for i in stride(from: -6, to: 1479, by: 33) {rightWall.append([Float(2274), Float(i)]) }                                  
+        for i in stride(from: -6, to: 1479, by: 33) {rightWall.append([Float(2274), Float(i)]) } - Cannot use mutating member on immutable value: 'leftWall' is a 'let' constant                                 
 
 
 
-      XCTAssertTrue(gameScene.walls(points:leftWall))
-      XCTAssertTrue(gameScene.walls(points:rightWall))
+      XCTAssertTrue(gameScene.walls(points:leftWall)) - Argument passed to call that takes no arguments
+      XCTAssertTrue(gameScene.walls(points:rightWall)) - Argument passed to call that takes no arguments
 
   
                      
@@ -34,11 +34,11 @@ func testTimer(){
       
       let gameScene=GameScene(size: CGSize(width: 2304 , height: 1440))
       
-      let wait = SKAction.wait(forDuration: 0.01)
-      let sequence = SKAction.sequence([call, SKAction.wait(forDuration: 0.02)])
+      let wait = SKAction.wait(forDuration: 0.01) - Use of unresolved identifier 'SKAction'
+      let sequence = SKAction.sequence([call, SKAction.wait(forDuration: 0.02)]) - Use of unresolved identifier 'SKAction'
       
-      XCTAssertTrue(gameScene.timer(wait:wait))
-      XCTAssertTrue(gameScene.timer(sequence:sequence))
+      XCTAssertTrue(gameScene.timer(wait:wait)) - Argument passed to call that takes no arguments
+      XCTAssertTrue(gameScene.timer(sequence:sequence)) - Argument passed to call that takes no arguments
       
 }      
       
