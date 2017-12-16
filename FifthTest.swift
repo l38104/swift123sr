6fd1d98f52372193722bcba6de35930754c55adf
l38046 <32620230+l38046@users.noreply.github.com>
2017-12-16 01:07:22 +0000

    Create FifthTest.swift

---
+++ FifthTest.swift
@@ -0,0 +1,16 @@
+func testPlayerPosition(){
+       
+       let gameScene=GameScene()
+       let pos=CGPoint(x:0,y:0)
+       
+       XCTAssertFalse(gameScene.player(palyer.postion:pos))
+      
+
+}
+
+
+
+
+
+
+
