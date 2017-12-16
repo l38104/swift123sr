8ab06ef540d44bb8bb321e36a528d34e263a65f2
l38104 <32617112+l38104@users.noreply.github.com>
2017-12-16 12:57:44 +0000

    Update FourthTest.swift

--- FourthTest.swift
+++ FourthTest.swift
@@ -1,12 +1,13 @@
 func testLoseLife(){
 
-      let gameScene=GameScene()
+      let gameScene=GameScene()  - emfalta: size: CGSize(width: 2304, height: 1440)
+        
       
       let life = 2
       let death=0
       let erro=-1
       
-      XCTAssertTrue(gameScene.loseLife(lifeNumber:life)
-      XCTAssertTrue(gameScene.loseLife(lifeNumber:death)
-      XCTAssertFalse(gameScene.loseLife(lifeNumber:erro)
+      XCTAssertTrue(gameScene.loseLife(lifeNumber:life) - Argument passed to call that takes no arguments
+      XCTAssertTrue(gameScene.loseLife(lifeNumber:death) - Argument passed to call that takes no arguments
+      XCTAssertFalse(gameScene.loseLife(lifeNumber:erro) - Argument passed to call that takes no arguments
 }
