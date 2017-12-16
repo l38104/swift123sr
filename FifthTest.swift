82e716466453219a5becb9af6b0f26450310aa6f
l38104 <32617112+l38104@users.noreply.github.com>
2017-12-16 13:09:46 +0000

    Update FifthTest.swift

--- FifthTest.swift
+++ FifthTest.swift
@@ -1,15 +1,14 @@
-func testPlayerPosition(){
-       
-       let gameScene=GameScene()
-       let pos=CGPoint(x:0,y:0)
-       
-       XCTAssertFalse(gameScene.player(palyer.postion:pos))
-      
-
-}
-
-
-
+     func testPlayerPosition(){
+            
+            let gameScene=GameScene( size: CGSize(width: 2304, height: 1440))
+            let pos=CGPoint(x:0,y:0)
+            
+            gameScene.player()
+            
+            XCTAssertFalse(gameScene.playerShip.position == pos )
+            
+            
+        }
 
 
 
