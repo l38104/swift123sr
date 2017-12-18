03c73397ddaa7b389e038dc623269ee0d15f1bda
l38104 <32617112+l38104@users.noreply.github.com>
2017-12-18 16:22:00 +0000

    Update SixthTest.swift

--- SixthTest.swift
+++ SixthTest.swift
@@ -13,3 +13,5 @@ func testAddObstacle(){
      
      XCTAssertTrue(gameScene.points == esquerda )
      XCTAssertTrue(gameScene.points == direita)
+
+     nao funciona: esquerda= [[Float(70),Float(1473)]] nao da o array que se quer testar
